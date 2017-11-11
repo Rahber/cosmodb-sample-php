5271f9608add27036e489f231c6692b4d945706f
Rahber <rahber50@gmail.com>
2017-11-11 12:52:55 +0400

    Initial commit

---
+++ var.php
@@ -0,0 +1,22 @@
+<?php
+
+/** Rahber ashraf
+www.rahber.net
+	*/
+
+
+	//edit only these variables
+	
+	$databaseAccount = "ratest";
+	$databaseAuthorizaiton="gIyZ186hK3QalxBMx8R0FJjzTIJuKr9PSKbBDaTNrJw4PCEj0NqfDPtQQQ3qeIXrgtdx0QoxMKmjuLZeralc6Q==";
+	$queryContentType = "application/json";
+	
+	
+	
+	//Do not edit the below.
+	
+	$databaseAccountURL = "https://".$databaseAccount.".documents.azure.com/dbs";
+	$curl = curl_init();
+	
+
+?>
